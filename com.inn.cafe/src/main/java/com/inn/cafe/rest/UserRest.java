37324646096855866b3efabfb06e53afcd4e4493
Franklin Manao <batuken.manao@gmail.com>
2024-11-04 23:24:51 +0800

    Updated dependencies.

--- com.inn.cafe/src/main/java/com/inn/cafe/rest/UserRest.java
+++ com.inn.cafe/src/main/java/com/inn/cafe/rest/UserRest.java
@@ -10,6 +10,6 @@ import java.util.Map;
 @RequestMapping(path = "user")
 public interface UserRest {
 
-    @PostMapping(path = "/signup")
+    @PostMapping(path = "signup")
     public ResponseEntity<String> signUp(@RequestBody(required = true)Map<String, String> requestMap);
 }
